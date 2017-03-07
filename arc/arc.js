d5691c55f78eb899d7bc3f61ec5919cb721bfaf3
Dan Lopez <dclopez@catalystdevworks.com>
2017-03-07 07:05:02 -0800

    Fix bad test, add more tests

--- arc/arc.js
+++ arc/arc.js
@@ -1,7 +1,7 @@
 export default class Arc {
-  constructor() {
-    this.originalWidth = 0;
-    this.originalHeight = 0;
+  constructor(width, height) {
+    this.originalWidth = typeof width === 'number' ? width : 0;
+    this.originalHeight = typeof height === 'number' ? height : 0;
   }
   setOriginalWidth(originalWidth) {
     this.originalWidth = originalWidth;
@@ -9,13 +9,25 @@ export default class Arc {
   setOriginalHeight(originalHeight) {
     this.originalHeight = originalHeight;
   }
+  calculateAspectRatio() {
+    return parseFloat((this.originalWidth / this.originalHeight).toFixed(3));
+  }
   calculateNewHeight(newWidth) {
     return this.calculateAspectRatio() * newWidth;
   }
   calculateNewWidth(newHeight) {
     return this.calculateAspectRatio() * newHeight;
   }
-  calculateAspectRatio() {
-    return (this.originalHeight / this.originalWidth);
+  getAspectRatioText() {
+    return ;
+  }
+  calculateGreatestCommonDivisor(x, y) {
+    if (typeof x !== 'number' || typeof y !== 'number') {
+      throw new Error('Must supply number parameters', x, y);
+    }
+    if (y === 0) {
+      return x;
+    }
+    return this.calculateGreatestCommonDivisor(y, x % y);
   }
 }
--- arc/arc.spec.js
+++ arc/arc.spec.js
@@ -5,7 +5,66 @@ describe('Arc', function () {
   describe('#Arc()', function () {
     it('should return a new Arc object', function () {
       const testArc = new Arc();
-      assert.typeOf(testArc, 'Arc');
+      assert.instanceOf(testArc, Arc);
+    });
+    it('sets the originalWidth to 0 if none is supplied', function () {
+      const testArc = new Arc();
+      assert.strictEqual(testArc.originalWidth, 0);
+    });
+    it('sets the originalHeight to 0 if none is supplied', function () {
+      const testArc = new Arc();
+      assert.strictEqual(testArc.originalHeight, 0);
+    });
+    it('sets the originalWidth to the value supplied', function() {
+      const testWidth = 10;
+      const testArc = new Arc(testWidth, null);
+      assert.strictEqual(testArc.originalWidth, testWidth);
+    });
+    it('sets the originalHeight to the value supplied', function() {
+      const testHeight = 10;
+      const testArc = new Arc(null, testHeight);
+      assert.strictEqual(testArc.originalHeight, testHeight);
+    });
+    it('sets the originalWidth to 0 if the value supplied is not a number', function () {
+      const badWidth = '';
+      const testArc = new Arc(badWidth, null);
+      assert.strictEqual(testArc.originalWidth, 0);
+    });
+    it('sets the originalHeight to 0 if the value supplied is not a number', function () {
+      const badHeight = '';
+      const testArc = new Arc(null, badHeight);
+      assert.strictEqual(testArc.originalHeight, 0);
+    });
+  });
+  describe('#setOriginalWidth(originalWidth)', function () {
+    it('sets the originalWidth to the given value', function () {
+      const testArc = new Arc();
+      const testWidth = 10;
+      testArc.setOriginalWidth(testWidth);
+      assert.strictEqual(testArc.originalWidth, testWidth);
+    });
+  });
+  describe('#setOriginalHeight(originalHeight)', function () {
+    it('sets the originalHeight to the given value', function () {
+      const testArc = new Arc();
+      const testHeight = 10;
+      testArc.setOriginalHeight(testHeight);
+      assert.strictEqual(testArc.originalHeight, testHeight);
+    });
+  });
+  describe('#calculateAspectRatio', function () {
+    it('divides the originalWidth by the originalHeight and limits the result to three decimal places', function () {
+      const testArc = new Arc(1920, 1080);
+      const expectedRatio = 1.778;
+      assert.strictEqual(testArc.calculateAspectRatio(), expectedRatio);
+    });
+  });
+  describe('#calculateGreatestCommonDivisor', function () {
+    it('returns the largest number that both the height and width can be divided by', function () {
+      const testArc = new Arc(1920, 1080);
+      const expectedGCD = 120;
+      const actualGCD = testArc.calculateGreatestCommonDivisor();
+      assert.strictEqual(actualGCD, expectedGCD);
     });
   });
 });
